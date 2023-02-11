51f70c0510af2b51fbf811ac8036ea971ac7b102
S Saber Basha <saberbasha752002@gmail.com>
2023-02-11 20:33:40 +0530

    added scrollview in news info

--- component/NewsInfo.js
+++ component/NewsInfo.js
@@ -1,52 +1,61 @@
 import React from "react";
-import { Image, Linking, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  Linking,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import AppButton from "./AppButton";
 import Icon from "./Icon";
 const ListingDetails = ({ route }) => {
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: route.params.urlToImage }} />
-      <View style={styles.detailsContainer}>
-        <Text style={styles.title}>{route.params.title}</Text>
-        <Text style={styles.subtitle}>{route.params.content}</Text>
-        <View style={styles.shareContainer}>
-          <View style={styles.iconContainer}>
-            <Icon
-              onPress={() =>
-                Linking.openURL(`whatsapp://send?text=${route.params.url}`)
-              }
-              name={"whatsapp"}
-              size={40}
-              backgroundColor="#00cc00"
-            />
-            <Text style={styles.logoText}>Whats App</Text>
+      <ScrollView>
+        <Image style={styles.image} source={{ uri: route.params.urlToImage }} />
+        <View style={styles.detailsContainer}>
+          <Text style={styles.title}>{route.params.title}</Text>
+          <Text style={styles.subtitle}>{route.params.content}</Text>
+          <View style={styles.shareContainer}>
+            <View style={styles.iconContainer}>
+              <Icon
+                onPress={() =>
+                  Linking.openURL(`whatsapp://send?text=${route.params.url}`)
+                }
+                name={"whatsapp"}
+                size={40}
+                backgroundColor="#00cc00"
+              />
+              <Text style={styles.logoText}>Whats App</Text>
+            </View>
+            <View style={styles.iconContainer}>
+              <Icon
+                onPress={() => Linking.openURL(`sms:?body=${route.params.url}`)}
+                name={"message"}
+                size={40}
+                backgroundColor="gold"
+              />
+              <Text style={styles.logoText}>Message</Text>
+            </View>
+            <View style={styles.iconContainer}>
+              <Icon
+                onPress={() =>
+                  Linking.openURL(`mailto:?&body=${route.params.url}`)
+                }
+                name={"email"}
+                size={40}
+                backgroundColor="#3399ff"
+              />
+              <Text style={styles.logoText}>Email</Text>
+            </View>
           </View>
-          <View style={styles.iconContainer}>
-            <Icon
-              onPress={() => Linking.openURL(`sms:?body=${route.params.url}`)}
-              name={"message"}
-              size={40}
-              backgroundColor="gold"
-            />
-            <Text style={styles.logoText}>Message</Text>
-          </View>
-          <View style={styles.iconContainer}>
-            <Icon
-              onPress={() =>
-                Linking.openURL(`mailto:?&body=${route.params.url}`)
-              }
-              name={"email"}
-              size={40}
-              backgroundColor="#3399ff"
-            />
-            <Text style={styles.logoText}>Email</Text>
-          </View>
+          <AppButton
+            onPress={() => Linking.openURL(route.params.url)}
+            title={"Click here to read full news"}
+          />
         </View>
-        <AppButton
-          onPress={() => Linking.openURL(route.params.url)}
-          title={"Click here to read full news"}
-        />
-      </View>
+      </ScrollView>
     </View>
   );
 };
@@ -56,6 +65,7 @@ const styles = StyleSheet.create({
     height: 250,
   },
   container: {
+    flex: 1,
     paddingBottom: 50,
   },
   detailsContainer: { padding: 20 },
@@ -80,6 +90,7 @@ const styles = StyleSheet.create({
   },
   logoText: {
     marginVertical: 5,
+    fontSize: 10,
   },
 });
 export default ListingDetails;
