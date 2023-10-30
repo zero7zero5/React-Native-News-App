f614cebc3ae627fdb646f45ad53c25e3a078bf73
S SABER BASHA <92513033+zero7zero5@users.noreply.github.com>
2023-10-30 10:55:23 +0530

    Update getNews.js

--- api/getNews.js
+++ api/getNews.js
@@ -1,11 +1,11 @@
 import api from "./client";
 const getNews = () => {
   return api.get(
-    "/top-headlines?country=in&apiKey=ec1cd982aae64a2faee053fe4e93826c"
+    "/top-headlines?country=in&apiKey=${Your_apikey}"
   );
 };
 const searchedNews = (str) => {
-  return api.get(`everything?q=${str}&apiKey=ec1cd982aae64a2faee053fe4e93826c`);
+  return api.get(`everything?q=${str}&apiKey=${Your_apikey}`);
 };
 export default {
   getNews,
